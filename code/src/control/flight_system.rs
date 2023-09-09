4f4af12a8ba7d25c40381c0f49ba9d31efb35e37
Dezash1 <desmondmehta@gmail.com>
2023-09-08 22:05:51 -0400

    order makes sense now

--- code/src/control/flight_system.rs
+++ code/src/control/flight_system.rs
@@ -110,116 +110,7 @@ impl FlightSystem {
             last_error_twist: 0.0,
         }
     }
-    fn core0_task(&mut self) -> ! {
-        loop {
-            let mut current_state = None;
-            unsafe {
-                let _clock = CoreStateLock::claim();
-                current_state = CORESTATE.clone();
-            }
-            let current_state = current_state.unwrap();
-            let a = current_state.last_acc.0;
-            let mut dtheta = current_state.last_gyr.0;
-            for i in 0..3 {
-                dtheta[i] -= self.mu_theta[i];
-            }
-            let adt = (current_state.last_acc.1 - self.last_acc_time) as f32 / 1000000.0;
-            self.last_acc_time = current_state.last_acc.1;
-            let tdt = (current_state.last_gyr.1 - self.last_gyr_time) as f32 / 1000000.0;
-            self.last_gyr_time = current_state.last_gyr.1;
-            let throttle = current_state.last_input.z_throttle;
-            match current_state.current_command {
-                DroneCommand::FullManual => self.full_manual(current_state.last_input),
-                DroneCommand::NormalControl => {
-                    let desired_angle: [f32; 2] = [
-                        current_state.last_input.x_throttle * MAX_TILT,
-                        current_state.last_input.y_throttle * MAX_TILT,
-                    ];
-                    // ignored for now
-                    let desired_twist: f32 = current_state.last_input.twist_throttle * MAX_TWIST;
-                    self.normal_control(throttle, desired_angle, desired_twist, a, dtheta, adt, tdt)
-                }
-                _ => self.normal_control(throttle, [0.0; 2], 0.0, a, dtheta, adt, tdt),
-            }
-        }
-    }
-    fn normal_control(
-        &mut self,
-        throttle: f32,
-        desired_angle: [f32; 2],
-        desired_twist: f32,
-        a: [f32; 3],
-        dtheta: [f32; 3],
-        adt: f32,
-        tdt: f32,
-    ) {
-        let mut new_speeds = [throttle; 4];
-        // probably need kalman filter and dtheta influence for v_xy > 0
-        let measured_angle = cartesian_to_polar(a, true);
 
-        let error_x = desired_angle[0] - measured_angle[0];
-        let error_y = desired_angle[1] - measured_angle[1];
-        let error_twist = desired_twist - dtheta[2];
-
-        let p_x = KPX * error_x;
-        let p_y = KPY * error_x;
-        let p_twist = KPTWIST * error_twist;
-
-        // use setpoint velocity and measured omega instead????
-        let d_x = KDX * (error_x - self.last_error_x) / adt;
-        let d_y = KDY * (error_y - self.last_error_y) / adt;
-        let d_twist = KDTWIST * (error_twist - self.last_error_twist) / tdt;
-
-        self.last_error_x = error_x;
-        self.last_error_y = error_y;
-        self.last_error_twist = error_twist;
-
-        let i_x = KIX * error_x * adt + self.last_integral_x;
-        let i_y = KIY * error_y * adt + self.last_integral_y;
-        let i_twist = KITWIST * error_twist * tdt + self.last_integral_twist;
-
-        self.last_integral_x = i_x;
-        self.last_integral_y = i_y;
-        self.last_integral_twist = i_twist;
-
-        let all_x = p_x + d_x + i_x;
-        let all_y = p_y + d_y + i_y;
-        let all_twist = p_twist + d_twist + i_twist;
-
-        new_speeds[0] += all_x + all_y + all_twist;
-        new_speeds[1] += -all_x + all_y - all_twist;
-        new_speeds[2] += -all_x - all_y + all_twist;
-        new_speeds[3] += all_x - all_y - all_twist;
-
-        self.set_speeds(new_speeds);
-    }
-    fn full_manual(&mut self, command: RadioCommand) {
-        // full manual control
-        let mut speeds: [f32; 4] = [command.z_throttle; 4];
-        speeds[0] += MAX_THROTTLE_DIFFERENCE * (command.x_throttle + command.y_throttle);
-        speeds[1] += MAX_THROTTLE_DIFFERENCE * (command.x_throttle + command.y_throttle);
-        speeds[2] += MAX_THROTTLE_DIFFERENCE * (command.x_throttle + command.y_throttle);
-        speeds[3] += MAX_THROTTLE_DIFFERENCE * (command.x_throttle + command.y_throttle);
-        // i dont even remember how this works
-        let max = *speeds
-            .iter()
-            .max_by(|&a, &b| a.partial_cmp(b).unwrap_or(core::cmp::Ordering::Equal))
-            .unwrap_or(&0.0);
-        if max == 0.0 {
-            self.set_speeds([0.0; 4]);
-        } else {
-            for i in 0..4 {
-                speeds[i] /= max;
-            }
-            self.set_speeds(speeds);
-        }
-    }
-    fn fall_out_of_the_sky(&mut self) -> ! {
-        self.set_speeds([0.0; 4]);
-        loop {
-            panic!("AAAAAAaaAAAAAAAaAAaa");
-        }
-    }
     pub fn start(
         &mut self,
         mut delay: cortex_m::delay::Delay,
@@ -269,20 +160,7 @@ impl FlightSystem {
         });
         self.core0_task();
     }
-    fn calc_speeds(&self, desired_speeds: [f32; 3], desired_rotations: [f32; 3]) -> [u16; 4] {
-        [0, 0, 0, 0]
-    }
-    fn set_speeds(&mut self, speeds: [f32; 4]) {
-        // clockwise starting at FL
-        let mut speedsu16: [u16; 4] = [0; 4]; //inefficient!!
-        for i in 0..4 {
-            speedsu16[i] = (speeds[i] * MAX_THROTTLE as f32) as u16;
-        }
-        self.fl.set_duty(speedsu16[0]);
-        self.fr.set_duty(speedsu16[1]);
-        self.br.set_duty(speedsu16[2]);
-        self.bl.set_duty(speedsu16[3]);
-    }
+
     fn core1_task(
         //read from stuff and update states
         mut delay: cortex_m::delay::Delay,
@@ -335,6 +213,135 @@ impl FlightSystem {
                 let _clock = CoreStateLock::claim();
                 CORESTATE = Some(newstate);
             }
+        }
+    }
+
+    fn core0_task(&mut self) -> ! {
+        loop {
+            let mut current_state = None;
+            unsafe {
+                let _clock = CoreStateLock::claim();
+                current_state = CORESTATE.clone();
+            }
+            let current_state = current_state.unwrap();
+            let a = current_state.last_acc.0;
+            let mut dtheta = current_state.last_gyr.0;
+            for i in 0..3 {
+                dtheta[i] -= self.mu_theta[i];
+            }
+            let adt = (current_state.last_acc.1 - self.last_acc_time) as f32 / 1000000.0;
+            self.last_acc_time = current_state.last_acc.1;
+            let tdt = (current_state.last_gyr.1 - self.last_gyr_time) as f32 / 1000000.0;
+            self.last_gyr_time = current_state.last_gyr.1;
+            let throttle = current_state.last_input.z_throttle;
+            match current_state.current_command {
+                DroneCommand::FullManual => self.full_manual(current_state.last_input),
+                DroneCommand::NormalControl => {
+                    let desired_angle: [f32; 2] = [
+                        current_state.last_input.x_throttle * MAX_TILT,
+                        current_state.last_input.y_throttle * MAX_TILT,
+                    ];
+                    // ignored for now
+                    let desired_twist: f32 = current_state.last_input.twist_throttle * MAX_TWIST;
+                    self.normal_control(throttle, desired_angle, desired_twist, a, dtheta, adt, tdt)
+                }
+                DroneCommand::Hover => {
+                    self.normal_control(throttle, [0.0; 2], 0.0, a, dtheta, adt, tdt)
+                }
+                DroneCommand::Calibrate => continue,
+                _ => self.fall_out_of_the_sky(),
+            }
+        }
+    }
+
+    fn normal_control(
+        &mut self,
+        throttle: f32,
+        desired_angle: [f32; 2],
+        desired_twist: f32,
+        a: [f32; 3],
+        dtheta: [f32; 3],
+        adt: f32,
+        tdt: f32,
+    ) {
+        let mut new_speeds = [throttle; 4];
+        // probably need kalman filter and dtheta influence for v_xy > 0
+        let measured_angle = cartesian_to_polar(a, true);
+
+        let error_x = desired_angle[0] - measured_angle[0];
+        let error_y = desired_angle[1] - measured_angle[1];
+        let error_twist = desired_twist - dtheta[2];
+
+        let p_x = KPX * error_x;
+        let p_y = KPY * error_x;
+        let p_twist = KPTWIST * error_twist;
+
+        // use setpoint velocity and measured omega instead????
+        let d_x = KDX * (error_x - self.last_error_x) / adt;
+        let d_y = KDY * (error_y - self.last_error_y) / adt;
+        let d_twist = KDTWIST * (error_twist - self.last_error_twist) / tdt;
+
+        self.last_error_x = error_x;
+        self.last_error_y = error_y;
+        self.last_error_twist = error_twist;
+
+        let i_x = KIX * error_x * adt + self.last_integral_x;
+        let i_y = KIY * error_y * adt + self.last_integral_y;
+        let i_twist = KITWIST * error_twist * tdt + self.last_integral_twist;
+
+        self.last_integral_x = i_x;
+        self.last_integral_y = i_y;
+        self.last_integral_twist = i_twist;
+
+        let all_x = p_x + d_x + i_x;
+        let all_y = p_y + d_y + i_y;
+        let all_twist = p_twist + d_twist + i_twist;
+
+        new_speeds[0] += all_x + all_y + all_twist;
+        new_speeds[1] += -all_x + all_y - all_twist;
+        new_speeds[2] += -all_x - all_y + all_twist;
+        new_speeds[3] += all_x - all_y - all_twist;
+
+        self.set_speeds(new_speeds);
+    }
+
+    fn set_speeds(&mut self, speeds: [f32; 4]) {
+        // clockwise starting at FL
+        let mut speedsu16: [u16; 4] = [0; 4]; //inefficient!!
+        for i in 0..4 {
+            speedsu16[i] = (speeds[i] * MAX_THROTTLE as f32) as u16;
+        }
+        self.fl.set_duty(speedsu16[0]);
+        self.fr.set_duty(speedsu16[1]);
+        self.br.set_duty(speedsu16[2]);
+        self.bl.set_duty(speedsu16[3]);
+    }
+
+    fn full_manual(&mut self, command: RadioCommand) {
+        let mut speeds: [f32; 4] = [command.z_throttle; 4];
+        speeds[0] += MAX_THROTTLE_DIFFERENCE * (command.x_throttle + command.y_throttle);
+        speeds[1] += MAX_THROTTLE_DIFFERENCE * (command.x_throttle + command.y_throttle);
+        speeds[2] += MAX_THROTTLE_DIFFERENCE * (command.x_throttle + command.y_throttle);
+        speeds[3] += MAX_THROTTLE_DIFFERENCE * (command.x_throttle + command.y_throttle);
+        // i dont even remember how this works
+        let max = *speeds
+            .iter()
+            .max_by(|&a, &b| a.partial_cmp(b).unwrap_or(core::cmp::Ordering::Equal))
+            .unwrap_or(&0.0);
+        if max == 0.0 {
+            self.set_speeds([0.0; 4]);
+        } else {
+            for i in 0..4 {
+                speeds[i] /= max;
+            }
+            self.set_speeds(speeds);
+        }
+    }
+
+    fn fall_out_of_the_sky(&mut self) -> ! {
+        self.set_speeds([0.0; 4]);
+        loop {
+            panic!("AAAAAAaaAAAAAAAaAAaa");
         }
     }
 }
